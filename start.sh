416c470e6c3f9a367e4c9d163ad0940a857e6358
Thomas Deutsch <thomas@tuxpeople.org>
2021-01-06 20:40:10 +0100

    add the possibility to provider a prepared .google_authenticator

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@
 [[ -e /authorized_keys ]] \
 	&& cp /authorized_keys /bastion/ \
 	&& chown bastion:users /bastion/authorized_keys
-
+[[ -e /.google_authenticator ]] \
+	&& cp /.google_authenticator /bastion/ \
+	&& chown bastion:users /bastion/.google_authenticator \
+	&& chmod 0400 /bastion/.google_authenticator
+	
 ssh-keygen -A \
 && exec /usr/sbin/sshd -De -o LogLevel=$LOG_LEVEL
