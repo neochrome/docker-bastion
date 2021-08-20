98c1b70fd0e6eb762d60c5056700536bc124465f
neochrome <johan@stenqvist.net>
2021-08-20 14:30:53 +0200

    add notifications about applied settings on start

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 [[ -e /motd ]] \
+	&& echo "Applying supplied motd" \
 	&& cp /motd /etc/
 [[ -e /authorized_keys ]] \
+	&& echo "Applying supplied authorized_keys" \
 	&& cp /authorized_keys /bastion/ \
 	&& chown bastion:users /bastion/authorized_keys
 [[ -e /.google_authenticator ]] \
+	&& echo "Applying supplied google authenticator settings" \
 	&& cp /.google_authenticator /bastion/ \
 	&& chown bastion:users /bastion/.google_authenticator \
 	&& chmod 0400 /bastion/.google_authenticator
